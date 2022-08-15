ecfc593f5cab3a36720f84c418845913ff506508
Kacper <kacper.milejski.git@gmail.com>
2022-08-15 17:51:11 +0200

    Adding simple methods for Elevator class

--- Elevator.hpp
+++ Elevator.hpp
@@ -16,6 +16,19 @@ public:
     Elevator(int floor) : mFloor(floor){}
     int incrementFloor();
     int decrementFloor();
-
+    int getFloor();
 
 };
+
+
+int Elevator::getFloor(){
+    return 1;
+}
+
+int Elevator::incrementFloor(){
+    return 1;
+}
+
+int Elevator::decrementFloor(){
+    return 1;
+}
