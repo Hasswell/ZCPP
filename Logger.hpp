f07ee3c1a958ebef66be56f72fd9239851db70a5
Kacper <kacper.milejski.git@gmail.com>
2022-08-20 22:50:21 +0200

    Add simple logger for the logs

---
+++ Logger.hpp
@@ -0,0 +1,29 @@
+#include <mutex>
+#include <thread>
+#include <fstream>
+#include <filesystem>
+
+
+//one global object of Logger
+class Logger{
+    public:
+    Logger(const std::string fileName) : mFileName(fileName.c_str()){
+
+    }
+    void logMessage(std::string message);
+
+
+    private:
+        std::mutex mlocker;
+        std::ofstream mloggerFile;
+        std::string mFileName;
+
+};
+
+
+void Logger::logMessage(std::string message){
+    std::scoped_lock lockMessage(mlocker);
+    mloggerFile.open(mFileName,std::ios_base::app);
+    mloggerFile.write(message.data(),message.size());
+    mloggerFile.close();
+}
