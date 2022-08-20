b8ea4a997ea6e5ac711deeb10d0940943b2f1866
Kacper <kacper.milejski.git@gmail.com>
2022-08-20 20:01:36 +0200

    Removing UT scripter

--- UT_Scripter.sh
+++
@@ -1,2 +0,0 @@
-g++ UnitTests/ElevatorTests.cpp -lgtest -lpthread -o TEST_ELEVATOR && ./TEST_ELEVATOR
-g++ UnitTests/ElevatorCORTests.cpp -lgtest -lpthread -o TEST_ELEVATOR_COR && ./TEST_ELEVATOR_COR
