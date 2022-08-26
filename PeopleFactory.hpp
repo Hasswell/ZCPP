8db137d8e78f2d74330dba6113c76f54339a292d
Kacper <kacper.milejski.git@gmail.com>
2022-08-26 19:20:46 +0200

    Create People factory class

--- PeopleFactory.hpp
+++ PeopleFactory.hpp
@@ -18,11 +18,12 @@ People PeopleFactory::createRandom(int minFloor, int maxFloor){
     std::random_device randomDevice; 
     const int maxNumberOfPeople = 10;
     std::mt19937 generator(randomDevice()); 
-    std::uniform_int_distribution<> distributionForFloors(minFloor, maxFloor); 
-    std::uniform_int_distribution<> distributionForPeople(0, maxNumberOfPeople); 
+    std::uniform_int_distribution<> distributionForDestination(minFloor, maxFloor); 
+    std::uniform_int_distribution<> distributionForStarting(minFloor, maxFloor); 
+    std::uniform_int_distribution<> distributionForPeople(1, maxNumberOfPeople); 
     unsigned int numberOfPeople = (unsigned int)distributionForPeople(generator);
-    int destination = distributionForFloors(generator);
-    int startFloor = distributionForFloors(generator);
+    int destination = distributionForDestination(generator);
+    int startFloor = distributionForStarting(generator);
     return create(numberOfPeople,destination,startFloor);
 }
 
